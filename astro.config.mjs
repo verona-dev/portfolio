f7aa3fa72f1df36e3a04460d468bef9a57190153
verona-dev <68257343+verona-hub@users.noreply.github.com>
2025-04-14 16:50:39 +0200

    add sitemap integration to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    trailingSlash: 'never',
+    
+    integrations: [
+       sitemap(),
+    ],
+});
