284609d9c1cbf0b40c81290979a21231a1699fba
verona-dev <68257343+verona-hub@users.noreply.github.com>
2025-04-14 20:03:17 +0200

    install astro-icon module

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zorang.netlify.app',
-
-  // output: 'static',
-  // trailingSlash: 'never',
-
-  integrations: [
-     sitemap(),
-  ],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
-	experimental: {
-		svg: true,
-	},
+    site: 'https://zorang.netlify.app',
+    
+    // output: 'static',
+    // trailingSlash: 'never',
+    
+    integrations: [
+        icon(),
+        sitemap(),
+    ],
+    
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    experimental: {
+        svg: true,
+    },
 });
