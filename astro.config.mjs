c43532ebfdb7de52f905ff7582c45501e5951c39
verona-dev <68257343+verona-hub@users.noreply.github.com>
2025-04-14 19:40:01 +0200

    install Tailwind module

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zorang.netlify.app',
-    // output: 'static',
-    // trailingSlash: 'never',
-    
-    integrations: [
-       sitemap(),
-    ],
+  site: 'https://zorang.netlify.app',
+
+  // output: 'static',
+  // trailingSlash: 'never',
+
+  integrations: [
+     sitemap(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
