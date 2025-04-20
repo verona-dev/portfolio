b1ad80e6b32f42a08cbdd0302f647ecfb04ecfe1
verona-dev <68257343+verona-hub@users.noreply.github.com>
2025-04-21 00:50:01 +0200

    ProjectsItem card adjustment add profile-picture.jpg

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://zorang.netlify.app',
